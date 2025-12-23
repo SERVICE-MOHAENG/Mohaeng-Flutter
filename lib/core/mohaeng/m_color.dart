cac54aa8d4256c13473c58a9fdd27c4d8f704e3f
taejun <ptj01036251@gmail.com>
2025-12-23 20:29:28 +0900

    m_color; 컬러 시스템 적용

---
+++ lib/core/mohaeng/m_color.dart
@@ -0,0 +1,30 @@
+import 'dart:ui';
+
+class MColor {
+  //Primary
+  static Color primary100 = Color(0xffD2F7FF);
+  static Color primary200 = Color(0xff9EECFF);
+  static Color primary300 = Color(0xff69E2FF);
+  static Color primary400 = Color(0xff35D7FE);
+  static Color primary500 = Color(0xff00CCFF);
+
+  //Error
+  static Color error100 = Color(0xffF96464);
+  static Color error200 = Color(0xffF50D0D);
+  static Color error300 = Color(0xff9D0808);
+
+  //White & Black
+  static Color white100 = Color(0xffFFFFFF);
+  static Color black100 = Color(0xff000000);
+
+  //Gray
+  static Color gray100 = Color(0xffFAFAFA);
+  static Color gray200 = Color(0xffE4E4E7);
+  static Color gray300 = Color(0xffD4D4D8);
+  static Color gray400 = Color(0xffA1A1AA);
+  static Color gray500 = Color(0xff6B6B6B);
+  static Color gray600 = Color(0xff525252);
+  static Color gray700 = Color(0xff404040);
+  static Color gray800 = Color(0xff262626);
+  static Color gray900 = Color(0xff171717);
+}
