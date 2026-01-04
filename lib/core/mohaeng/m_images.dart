ec573468c1fa4e3e222e851461ae78e320ce4ac9
taejun <ptj01036251@gmail.com>
2026-01-04 23:26:33 +0900

    chore: 이미지 및 이미지 리소스 추가

---
+++ lib/core/mohaeng/m_images.dart
@@ -0,0 +1,10 @@
+class MImages {
+  MImages._();
+
+  static const String mohaengLogo = 'assets/images/logo/mohaeng_logo.png';
+
+  static const String kakaoLogo = 'assets/images/logo/kakao_logo.png';
+  static const String naverLogo = 'assets/images/logo/naver_logo.png';
+  static const String googleLogo = 'assets/images/logo/google_logo.png';
+
+}
