5ed760a08f78b50b566be38f2f6aa3ca7ec50f65
taejun <ptj01036251@gmail.com>
2026-01-06 19:37:41 +0900

    fix: bottomSheet 배경 색 변경

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mohaeng_app_service/core/constants/app_routes.dart';
+import 'package:mohaeng_app_service/core/mohaeng/m_color.dart';
 import 'package:mohaeng_app_service/features/auth/presentation/view/ui/login_screen.dart';
+import 'package:mohaeng_app_service/features/auth/presentation/view/ui/sign_up_screen.dart';
 import 'package:mohaeng_app_service/features/splash/presentation/ui/splash_screen.dart';
 
 Future<void> main() async {
@@ -18,13 +20,19 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: Size(375, 812),
       child: MaterialApp(
-        theme: ThemeData(fontFamily: 'Pretendard'),
+        theme: ThemeData(
+          fontFamily: 'Pretendard',
+          bottomSheetTheme: BottomSheetThemeData(
+            backgroundColor: MColor.white100,
+          ),
+        ),
         debugShowCheckedModeBanner: false,
         initialRoute: AppRoutes.splash,
         routes: {
           AppRoutes.splash: (_) => SplashScreen(),
           AppRoutes.login: (_) => LoginScreen(),
-        }
+          AppRoutes.signup: (_) => SignUpScreen(),
+        },
       ),
     );
   }
