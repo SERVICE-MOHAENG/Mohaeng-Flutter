c869d411cb192f3d410a457bf6e5e8a83b19228c
taejun <ptj01036251@gmail.com>
2026-01-13 10:26:45 +0900

    refactor: 쓸데없는 코드 넘김 방지

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:mohaeng_app_service/core/constants/app_routes.dart';
 import 'package:mohaeng_app_service/core/mohaeng/m_color.dart';
 import 'package:mohaeng_app_service/features/auth/presentation/view/ui/login_screen.dart';
 import 'package:mohaeng_app_service/features/auth/presentation/view/ui/sign_up_screen.dart';
+import 'package:mohaeng_app_service/features/main/presentation/view/ui/main_screen.dart';
 import 'package:mohaeng_app_service/features/splash/presentation/ui/splash_screen.dart';
 
 Future<void> main() async {
@@ -13,9 +14,7 @@ Future<void> main() async {
 
   await dotenv.load(fileName: '.env');
 
-  KakaoSdk.init(
-    nativeAppKey: dotenv.env['KAKAO_NATIVE_KEY'],
-  );
+  KakaoSdk.init(nativeAppKey: dotenv.env['KAKAO_NATIVE_KEY']);
 
   runApp(MyApp());
 }
@@ -40,6 +39,7 @@ class MyApp extends StatelessWidget {
           AppRoutes.splash: (_) => SplashScreen(),
           AppRoutes.login: (_) => LoginScreen(),
           AppRoutes.signup: (_) => SignUpScreen(),
+          AppRoutes.main: (_) => MainScreen(),
         },
       ),
     );
