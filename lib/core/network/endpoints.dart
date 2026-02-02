d39d212e098c8ab469ac2de65ad5aed0bd274392
taejun <ptj01036251@gmail.com>
2026-02-02 12:30:56 +0900

    feat(network): 공통 네트워크(ApiClient/인터셉터/에러) 추가

---
+++ lib/core/network/endpoints.dart
@@ -0,0 +1,7 @@
+abstract final class ApiEndpoints {
+  static const String basePath = '/api/v1';
+
+  static const String courses = '$basePath/courses';
+  static const String blogs = '$basePath/blogs';
+  static const String visitedCountries = '$basePath/visited-countries';
+}
