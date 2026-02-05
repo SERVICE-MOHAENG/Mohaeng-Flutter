17404971a6caffcdc2dd1e250107e0dce7d21484
taejun <ptj01036251@gmail.com>
2026-02-05 10:15:36 +0900

    chore: 이미지 추가 및 리소스 설정

--- lib/core/mohaeng/m_images.dart
+++ lib/core/mohaeng/m_images.dart
@@ -10,6 +10,9 @@ class MImages {
   static const String blockImage = 'assets/images/circle_block.png';
   static const String completeIcon = 'assets/images/icons/complete.png';
   static const String sendIcon = 'assets/images/icons/send_icon.png';
+  static const String plusIcon = 'assets/images/icons/plus_icon.png';
+  static const String minusIcon = 'assets/images/icons/minus_icon.png';
+
 
   static const String japan = 'assets/images/country/japan.png';
   static const String america = 'assets/images/country/america.png';
