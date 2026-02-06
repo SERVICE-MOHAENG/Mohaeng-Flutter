05d996d09785eb61e8e4e6a6b5bfd4a9c41c1e2b
taejun <ptj01036251@gmail.com>
2026-02-06 10:04:38 +0900

    chore: 동행자 목록 이미지 추가

--- lib/core/mohaeng/m_images.dart
+++ lib/core/mohaeng/m_images.dart
@@ -13,6 +13,14 @@ class MImages {
   static const String plusIcon = 'assets/images/icons/plus_icon.png';
   static const String minusIcon = 'assets/images/icons/minus_icon.png';
 
+  static const String companionAlone = 'assets/images/companion/alone.png';
+  static const String companionParent = 'assets/images/companion/parent.png';
+  static const String companionFriend = 'assets/images/companion/friend.png';
+  static const String companionCouple = 'assets/images/companion/couple.png';
+  static const String companionBaby = 'assets/images/companion/baby.png';
+  static const String companionFamily = 'assets/images/companion/family.png';
+  static const String companionWorker = 'assets/images/companion/worker.png';
+
 
   static const String japan = 'assets/images/country/japan.png';
   static const String america = 'assets/images/country/america.png';
